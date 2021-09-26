6480c1cda2efd3cfb0d208fabf7192f27285df80
Cynthia Nosiri <cynthiasamuels98@gmail.com>
2021-09-25 21:31:33 -0400

    design changes

--- foodapi/src/Showrecipe.js
+++ foodapi/src/Showrecipe.js
@@ -4,7 +4,8 @@ export default function Showrecipe({recipe}) {
     return (
         recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null &&
         (
-        <div className="showrecipe-display" onClick={()=> window.open(recipe["recipe"]["url"])}>
+        <div className="showrecipe-display" onClick={()=> window.open(recipe["recipe"]["url"])}> 
+        {/* when the images are clicked then the full recipe opens up */}
             <div className="column">
             <img src={recipe["recipe"]["image"]} alt={recipe["recipe"]["label"]} className="images"/>
             <p>{recipe["recipe"]["label"]}</p>
